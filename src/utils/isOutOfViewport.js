69b247ca4f7b8086e7a79abc447888eace4fd8b2
Oleksandr Kuzmin <kuzmin@d-sire.com>
2021-11-19 12:46:44 +0100

    Fix of jumping container when scrolling with keyboard

--- src/utils/isOutOfViewport.js
+++ src/utils/isOutOfViewport.js
@@ -1,14 +1,16 @@
-export default function (elem) {
-    const bounding = elem.getBoundingClientRect();
+export default function (parent, container) {
+    const parentBounding = parent.getBoundingClientRect();
+    const boundingContainer = container.getBoundingClientRect();
     const out = {};
 
-    out.top = bounding.top < 0;
-    out.left = bounding.left < 0;
+    out.top = parentBounding.top < 0;
+    out.left = parentBounding.left < 0;
     out.bottom =
-        bounding.bottom + bounding.height >
+        parentBounding.bottom + boundingContainer.height >
         (window.innerHeight || document.documentElement.clientHeight);
+
     out.right =
-        bounding.right >
+        parentBounding.right >
         (window.innerWidth || document.documentElement.clientWidth);
     out.any = out.top || out.left || out.bottom || out.right;
 
