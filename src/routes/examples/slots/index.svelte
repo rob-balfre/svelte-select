494b341b0d5fcee779145f7b8614744f9f0dcfa7
Rob Balfre <robert.balfre@travelctm.com.au>
2022-08-11 09:27:14 +1000

    clean up

--- src/routes/examples/slots/index.svelte
+++
@@ -1,8 +0,0 @@
-<script context="module">
-    export async function load() {
-        return {
-            status: 301,
-            redirect: "/examples/props/append-list-target"
-        };
-    }
-</script>
