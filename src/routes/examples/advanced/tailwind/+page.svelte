c8112176cf06e834cce48d60624d4d2e03f662c8
Rob Balfre <robert.balfre@travelctm.com.au>
2022-10-14 08:53:25 +1100

    no-styles fix and example

--- src/routes/examples/advanced/tailwind/+page.svelte
+++ src/routes/examples/advanced/tailwind/+page.svelte
@@ -1,6 +1,12 @@
 <script>
     import Select from 'svelte-select/no-styles/Select.svelte';
 
+    /* 
+        Tailwind support is very experimental (lib author does not use it) and in need of some PRs to improve
+        import 'svelte-select/tailwind.css';
+        For sveltekit support see: https://tailwindcss.com/docs/guides/sveltekit
+    */
+
     let items = ['One', 'Two', 'Three'];
 </script>
 
