58673cb7b7c526048abc27e8f24a8c952dfc9ee8
Rob Balfre <robert.balfre@travelctm.com.au>
2022-11-02 06:49:56 +1100

    loadOptions example using fuse.js

---
+++ src/routes/examples/props/loadOptions/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Select from '$lib/Select.svelte';
+    import Fuse from 'fuse.js';
+
+    let items = ['one', 'two', 'three'];
+
+    async function handleOptions(filterText) {
+        const fuse = new Fuse([...items]);
+
+        return fuse.search(filterText).map(({ item }) => item);
+    }
+
+    function reset() {
+        items = [...items];
+    }
+</script>
+
+<Select {items} loadOptions={handleOptions} debounceWait="0" on:select={reset} on:clear={reset} />
