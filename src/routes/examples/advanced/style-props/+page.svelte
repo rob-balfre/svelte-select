28d884938769b463b67e76f2009ead9d2da92ef4
Rob Balfre <robert.balfre@travelctm.com.au>
2023-02-08 07:01:56 +1100

    style props example

---
+++ src/routes/examples/advanced/style-props/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Select from '$lib/Select.svelte';
+
+    let items = ['One', 'Two', 'Three'];
+    let listOpen = false;
+</script>
+
+<Select
+    bind:listOpen
+    {items}
+    listOffset={0}
+    --list-border-radius="0 0 5px 5px"
+    --item-first-border-radius="0"
+    --border-radius={listOpen ? '5px 5px 0 0' : '5px'} />
