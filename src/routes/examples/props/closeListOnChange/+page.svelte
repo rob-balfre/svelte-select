a8bdd0efc62efd5d3327def1a745cebaff740472
Rob Balfre <robert.balfre@travelctm.com.au>
2023-02-20 08:24:26 +1100

    closeListOnChange example

---
+++ src/routes/examples/props/closeListOnChange/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Select from '$lib/Select.svelte';
+
+    let items = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' },
+        { value: 'three', label: 'Three' },
+    ];
+</script>
+
+<Select {items} closeListOnChange={false} multiple />
+
