aec5cd4b906bb42dd2e857121a238c87c3cdfd61
Rob Balfre <robert.balfre@travelctm.com.au>
2023-04-02 07:23:41 +1000

    required example

---
+++ src/routes/examples/props/required/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Select from '$lib/Select.svelte';
+
+    let items = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' },
+        { value: 'three', label: 'Three' },
+    ];
+</script>
+
+<form on:submit|preventDefault>
+    <Select {items} required />
+    <button type="submit">Submit</button>
+</form>
