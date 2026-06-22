2c2c4fe645f3cb2c6244af63f6f91b822bdd489c
Simon Holthausen <simon.holthausen@vercel.com>
2026-06-22 23:48:11 +0200

    oh god

--- docs/generate_theming_variables_md.js
+++ docs/generate_theming_variables_md.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const SOURCE_FOLDER = path.join(__dirname, '..', 'src/lib');
 
 const DOC_FILE_PATH = path.join(__dirname, 'theming_variables.md');
-const VARIABLE_SECTION_PATTERN = /(<!-- List start -->)(.|\n)*(<!-- List end -->)/gm;
+const VARIABLE_SECTION_PATTERN = /(<!-- List start -->)[\s\S]*(<!-- List end -->)/m;
 
 function getSvelteFiles(dir) {
     const entries = fs.readdirSync(dir, { withFileTypes: true });
@@ -34,21 +34,17 @@ const uniqueMatches = [...new Set(justTheMatchedParts).values()];
 uniqueMatches.sort();
 const matchesAsMarkdownListItems = uniqueMatches.map((b) => b.replace(/var\((--[A-Za-z\-_]*)/, '- `$1`'));
 
-const START_TAG_CAPTURE_GROUP = '$1';
-const END_TAG_CAPTURE_GROUP = '$3';
-const newDependencySectionAsRegexReplaceExpression = [
-    START_TAG_CAPTURE_GROUP,
-    ...matchesAsMarkdownListItems,
-    END_TAG_CAPTURE_GROUP,
-].join('\n');
 const oldContent = fs.readFileSync(DOC_FILE_PATH, 'utf8');
+const line_ending = oldContent.includes('\r\n') ? '\r\n' : '\n';
 const oldFileDoesNotContainSection = oldContent.search(VARIABLE_SECTION_PATTERN) === -1;
 if (oldFileDoesNotContainSection) {
     console.error(`Could not find variable section in ${DOC_FILE_PATH}: ${oldContent}`);
     process.exit(1);
 }
-fs.writeFileSync(
-    DOC_FILE_PATH,
-    oldContent.replace(VARIABLE_SECTION_PATTERN, newDependencySectionAsRegexReplaceExpression)
+
+const replacement_section = ['<!-- List start -->', ...matchesAsMarkdownListItems, '<!-- List end -->'].join(
+    line_ending
 );
+
+fs.writeFileSync(DOC_FILE_PATH, oldContent.replace(VARIABLE_SECTION_PATTERN, replacement_section));
 console.log(`Successfully wrote to ${DOC_FILE_PATH}`);
